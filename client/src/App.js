f27c61f4b13b8fe399d92ab58d3a58d47e029d16
unknown <sebkpl@gmail.com>
2019-11-18 20:40:44 -0600

    Route setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import Trade from "./components/trade/Trade";
 
 const App = () => {
     return (
-        <div>
-            <h1>Welcome</h1>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/trade" component={Trade} />
+            </Switch>
+        </Router>
     );
 };
 
